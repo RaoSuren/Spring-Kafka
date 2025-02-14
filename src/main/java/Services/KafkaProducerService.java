518b2bc1ca43f5ffdc147b795f9c9a07385089d4
RaoSuren <surendraydv022@gmail.com>
2025-02-14 11:46:28 +0530

    Added Route and API End point for Sending Message to Kafka TOPIC and Producer Service

--- src/main/java/Services/KafkaProducerService.java
+++ src/main/java/Services/KafkaProducerService.java
@@ -1,4 +1,22 @@
 package Services;
 
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
 public class KafkaProducerService {
+
+    private final String TOPIC = "my_topic";
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate)
+    {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message)
+    {
+        kafkaTemplate.send(TOPIC, message);
+        System.out.println("Message sent: "+ message);
+    }
 }
--- src/main/java/controllers/KafkaController.java
+++ src/main/java/controllers/KafkaController.java
@@ -1,10 +1,23 @@
 package controllers;
 
 import Services.KafkaProducerService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class KafkaController {
 
     private KafkaProducerService producerService;
+
+    public KafkaController(KafkaProducerService producerService)
+    {
+        this.producerService = producerService;
+    }
+
+    @GetMapping("/send")
+    public String sendMessage(@RequestParam String message)
+    {
+        return "Message Sent Successfully";
+    }
 }
